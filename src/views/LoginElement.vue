9be187b048145b3c2611c1d047f1c7dfb1ee4c6f
mul <77082685+moonmul@users.noreply.github.com>
2021-10-30 01:07:08 +0900

    css구성

--- src/views/LoginElement.vue
+++ src/views/LoginElement.vue
@@ -1,6 +1,25 @@
 <template>
-    <div :style="{'background-image':'url(' + require('@/assets/images/ipad.png') + ')'}">
-        <img :src="require('@/assets/images/logo/process_logo.png')" />
+    <div>
+        <div class="sub-title">
+            <div class="circle"></div>
+            <f1>RESEARCH RECORD</f1>
+            <f2>지금까지 진행한 현황을 볼 수 있습니다.</f2>
+        </div>
+        <div class="calendar-pad" :style="{'background-image':'url(' + require('@/assets/images/ipad.png') + ')'}">
+            <div class="calendar-card">
+                <img :src="require('@/assets/images/logo/process_logo.png')" />
+                <hr>
+                STRETCHING CALENDAR
+                <div class="calender-card">
+
+                </div>
+
+                MISSION TEN TIMES
+                <div class="mission-card">
+                    <div class="circle">C</div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -12,3 +31,6 @@ export default {
     }
 }
 </script>
+<style lang="scss">
+@import '@/assets/scss/components/login.scss';
+</style>
