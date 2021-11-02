badd905105ffd78c1f8c794dba6ca98ae61b6908
Se0 <sy.park.HUB@gmail.com>
2021-11-02 12:39:44 +0900

    메인페이지 Header,Nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,82 +1,61 @@
 <template>
-    <div>
-        <h1>Welcome to {{title1}}!</h1>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <input type="text" v-model="input1"/>
-        <button type="button" @click="getData">Get</button>
-        <button type="button" @click="setData">Set</button>
-
-        <select class="form-control" v-model="region" @change="changeRegion">
-            <option :key="i" :value="d.v" v-for="(d,i) in options">{{d.t}}</option>
-        </select>
-
-        <table class="table table-bordered" v-show="tableShow"> <!-- v-if: false 일땐 랜더링X, v-show: 랜더링O, 화면에 보이고 안보이고를 조절 -->
-            <tr :key="i" v-for="(d,i) in options">
-                <td>{{d.v}}</td>
-                <td>{{d.t}}</td>
-            </tr>
-        </table>
+  <body>
+    <!-- 검은색 바 -->
+    <div class="bar">
+        <div class="music-player">▶ This is playing music</div>
+        <div class="clock"> 10 : 50 AM </div>
     </div>
+    <!-- 헤더(흰색) -->
+  <header class="header">
+    <div class="header-logo"><a href="#"><img src="..\assets\images\logo\logo.png"></a></div>
+    <nav>
+      <ul>
+        <a href="#" class="menu-item">
+          <li class="menu-text">스트레칭 가이드</li>
+          <img src="..\assets\icon\menu.png">
+        </a> 
+        <a href="#" class="menu-item">
+          <li class="menu-text">마이 루틴</li>
+          <img src="..\assets\icon\menu.png">
+        </a> 
+        <a href="#" class="menu-item">
+          <li class="menu-text"> 랜덤 챌린지</li>
+          <img src="..\assets\icon\menu.png">
+        </a> 
+        <a href="#" class="menu-item">
+          <li class="menu-text">이용안내</li>
+          <img src="..\assets\icon\menu.png">
+        </a> 
+      </ul>
+
+      <div class ="my-menu">
+        <a href="#" class="menu-text"><img src="../assets/icon/mypage.png"></a>
+        <a href="#" class="menu-text"><img src="../assets/icon/notification.png"></a>
+      </div>
+    </nav>
+  </header>
+  
+   <!-- banner -->
+  <div class="banner">
+  </div>
+  <!-- main -->
+  <div class="main">
+  </div>
+  <!-- 모래부분 -->
+  <footer class="foot">
+  </footer>
+
+  </body>
 </template>
+<style lang="scss" scoped>
+    @import '../assets/scss/components/main.scss';
+    @import '../assets/scss/components/header.scss';
+    @import '../assets/scss/variables.scss';
+    @import '../assets/scss/common.scss';
+</style>
 <script>
 export default {
     data() {
-        return {
-            title1: "BTP",
-            input1: "abcd",
-            options: [
-                {v:"S", t:"Seoul"},
-                {v:"J", t:"Jeju"}
-            ],
-            region: "J",
-            tableShow: false
-        };
     },
-    watch: {
-        input1() {
-            console.log(this.input1);
-        }
-    },
-    methods:{
-        getData() {
-            alert(this.input1);
-        },
-        setData() {
-            this.input1 = "12345";
-        },
-        changeRegion() {
-            alert(this.region);
-        }
-    },
-    beforeCreate() {
-        console.log("beforeCreate");
-    },
-    created() {
-        console.log("created");
-    },
-    beforeMount() {
-        console.log("beforeMount");
-    },
-    mounted() {
-        console.log("mounted");
-    },
-    beforeUpdate() {
-        console.log("beforeUpdate");
-    },
-    updated() {
-        console.log("updated");
-    },
-    beforeDestroy() {
-        console.log("beforeDestroy");
-    },
-    destroyed() {
-        console.log("destroyed");
-    }
 };
 </script>
