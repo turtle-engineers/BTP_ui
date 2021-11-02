85f5f697ef121a55e289d90cab2c07466091f7df
Se0 <sy.park.HUB@gmail.com>
2021-11-02 16:05:14 +0900

    메인페이지 프로젝트메뉴 레이아웃 (+ignore .vs)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,52 +6,91 @@
         <div class="clock"> 10 : 50 AM </div>
     </div>
     <!-- 헤더(흰색) -->
-  <header class="header">
-    <div class="header-logo"><a href="#"><img src="..\assets\images\logo\logo.png"></a></div>
-    <nav>
-      <ul>
-        <a href="#" class="menu-item">
-          <li class="menu-text">스트레칭 가이드</li>
-          <img src="..\assets\icon\menu.png">
-        </a> 
-        <a href="#" class="menu-item">
-          <li class="menu-text">마이 루틴</li>
-          <img src="..\assets\icon\menu.png">
-        </a> 
-        <a href="#" class="menu-item">
-          <li class="menu-text"> 랜덤 챌린지</li>
-          <img src="..\assets\icon\menu.png">
-        </a> 
-        <a href="#" class="menu-item">
-          <li class="menu-text">이용안내</li>
-          <img src="..\assets\icon\menu.png">
-        </a> 
-      </ul>
+    <header class="header">
+      <div class="header-logo"><a href="#"><img src="..\assets\images\logo\logo.png"></a></div>
+      <nav>
+        <ul>
+          <a href="#" class="menu-item">
+            <li class="menu-text">스트레칭 가이드</li>
+            <img src="..\assets\icon\menu.png">
+          </a> 
+          <a href="#" class="menu-item">
+            <li class="menu-text">마이 루틴</li>
+            <img src="..\assets\icon\menu.png">
+          </a> 
+          <a href="#" class="menu-item">
+            <li class="menu-text"> 랜덤 챌린지</li>
+            <img src="..\assets\icon\menu.png">
+          </a> 
+          <a href="#" class="menu-item">
+            <li class="menu-text">이용안내</li>
+            <img src="..\assets\icon\menu.png">
+          </a> 
+        </ul>
 
-      <div class ="my-menu">
-        <a href="#" class="menu-text"><img src="../assets/icon/mypage.png"></a>
-        <a href="#" class="menu-text"><img src="../assets/icon/notification.png"></a>
+        <div class ="my-menu">
+          <a href="#" class="menu-text"><img src="../assets/icon/mypage.png"></a>
+          <a href="#" class="menu-text"><img src="../assets/icon/notification.png"></a>
+        </div>
+      </nav>
+    </header>
+
+    <!-- banner -->
+    <div class="banner">
+      <img class="banner-img" src="../assets/images/banner.png">
+    </div>
+
+    <!-- main -->
+    <div class="main">
+      <!-- 브랜드 소개 -->
+      <div class="brand">
+        <div class="brand-img"><img src="../assets/images/brand.png"></div>
+        <div class="brand-desc">
+          <h1>BTP PROJECT</h1>
+          <p>
+            현대사회 사람은 지속해서 스트레칭 하지 않으면 거북이가 되어버립니다.<br>
+            현대사회 사람은 지속해서 스트레칭 하지 않으면 거북이가 되어버립니다.<br>
+            현대사회 사람은 지속해서 스트레칭 하지 않으면 거북이가 되어버립니다.<br>
+            현대사회 사람은 지속해서 스트레칭 하지 않으면 거북이가 되어버립니다.
+          </p>
+          <a href="#"><div class="morebtn">MORE</div></a>
+        </div>
       </div>
-    </nav>
-  </header>
-   <!-- banner -->
-  <div class="banner">
-    <img class="banner-img" src="../assets/images/banner.png">
-  </div>
-  <!-- main -->
-  <div class="main">
-    <!-- 브랜드 소개 -->
-    <div class="brand">
-      <div class="brand-img"><img src="../assets/images/brand.png"></div>
-      <div class="brand-desc">
-        <h1>BTP PROJECT</h1>
-        <p>
-          현대사회 사람은 지속해서 스트레칭 하지 않으면 거북이가 되어버립니다.<br>
-          현대사회 사람은 지속해서 스트레칭 하지 않으면 거북이가 되어버립니다.<br>
-          현대사회 사람은 지속해서 스트레칭 하지 않으면 거북이가 되어버립니다.<br>
-          현대사회 사람은 지속해서 스트레칭 하지 않으면 거북이가 되어버립니다.
-        </p>
-        <a href="#"><div class="morebtn">MORE</div></a>
+
+    <!-- 메인 프로젝트 -->
+    <div class="title">
+        <div class="circle"></div><h1>MAIN PROJECT</h1>
+        <p>본 프로젝트로 계획된 플랜을 살펴볼 수 있습니다.</p>
+    </div>
+
+    <div class="project-menu">     
+      <!-- 메뉴 -->
+      <div class="project-item">
+        <img class="project-icon" src="../assets/images/stretching.png">
+        <div class="project-desc">
+          <h1>스트레칭 가이드</h1>
+          <h2>My Stretching Routine</h2>
+        </div>
+        <div class="circle"></div>
+        <a href="#"><div class="start">START! ▶</div></a>
+      </div>
+      <div class="project-item">
+        <img class="project-icon" src="../assets/images/myroutine.png">
+        <div class="project-desc">
+          <h1>스트레칭 가이드</h1>
+          <h2>My Stretching Routine</h2>
+        </div>
+        <div class="circle"></div>
+        <a href="#"><div class="start">START! ▶</div></a>
+      </div>
+      <div class="project-item">
+        <img class="project-icon" src="../assets/images/random_challenge.png">
+        <div class="project-desc">
+          <h1>스트레칭 가이드</h1>
+          <h2>My Stretching Routine</h2>
+        </div>
+        <div class="circle"></div>
+        <a href="#"><div class="start">START! ▶</div></a>
       </div>
     </div>
   </div>
