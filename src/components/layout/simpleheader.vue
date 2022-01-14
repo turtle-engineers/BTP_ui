49e8f16dc729825c93610f1258b27543e2c6a188
Se0 <sy.park.HUB@gmail.com>
2022-01-15 01:03:50 +0900

    simpleheader (헤더 컴포넌트) now-page 삭제

---
+++ src/components/layout/simpleheader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header id="header">
+    <div id="header-box">
+      <nav>
+        <ul>
+          <a href="#" class="nav-item">
+            <li class="nav-text">스트레칭 가이드</li>
+          </a>
+          <a href="#" class="nav-item">
+            <li class="nav-text">마이 루틴</li>
+          </a>
+          <a href="/story" class="nav-item">
+            <li class="nav-text">프로젝트 소개</li>
+          </a>
+        </ul>
+      </nav>
+      <a href="/"><img class="logo" src="..\..\assets\images\logo\logo.svg"/></a>
+      <div class="my-menu">
+        <a href="#"><img src="../../assets/icon/mypage.svg"/></a>
+        <a href="#"><img src="../../assets/icon/notification.svg"/></a>
+      </div>
+    </div>
+  </header>
+  <!-- 현재 페이지에  id="now-page" -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import '../../assets/scss/variables.scss';
+@import '../../assets/scss/common.scss';
+@import 'simpleheader.scss';
+</style>
