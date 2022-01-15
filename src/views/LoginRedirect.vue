ce941c31399b89d6e20791d925e01ae1f73556c9
lianachoi <2ndsunmail@gmail.com>
2022-01-16 04:19:56 +0900

    로그인

---
+++ src/views/LoginRedirect.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="loginCheck">
+    {{ results }}
+    <br />
+    <h1>{{ results.nickname }}</h1>
+    <img :src="results.picture" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  created() {
+    this.create();
+  },
+  data() {
+    return {
+      sessionId: "",
+      results: {
+        id: "",
+        provider: "",
+        providerId: "",
+        picture: "",
+        nickname: "",
+        title: "",
+        level: "",
+        point: "",
+        createdAt: "",
+        updatedAt: "",
+        alarmValid: "",
+        monthTimes: "",
+        dd: "",
+        todayTimes: "",
+      },
+    };
+  },
+  methods: {
+    create() {
+      this.codes = this.$route.query.code;
+      this.getUser();
+    },
+    getUser() {
+      axios
+        .get(
+          "http://127.0.0.1:3000/oauth/kakao/callback?authorize_code=" +
+            this.codes
+        )
+        .then((res) => {
+          //   console.log(res);
+          document.cookie = `connect.sid=${res.data}`;
+          this.login();
+        });
+    },
+    login() {
+      //VueCookies.set("connect.sid", this.sessionId)
+      axios.get("http://127.0.0.1:3000/user/info").then((res) => {
+        if (res.data != null) {
+          this.results = res.data.results;
+          //   this.$router.push("/");
+        }
+        //   console.log(res);
+      });
+    },
+  },
+};
+</script>
