7fc4a94d2d1b9edc022ca36bb3b0f8e44aa0c4c1
lianachoi <2ndsunmail@gmail.com>
2022-01-16 04:19:56 +0900

    로그인

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login">
+    <img src="@/assets/images/turtle.png" @click="loginWithKakao()" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    loginWithKakao() {
+      const params = {
+        clientId: process.env.VUE_APP_CLIENT_KEY,
+        redirectUri: "http://127.0.0.1:8080/LoginRedirect",
+      };
+      window.location.replace(
+        "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" +
+          params.clientId +
+          "&redirect_uri=" +
+          params.redirectUri
+      );
+    },
+  },
+};
+</script>
