bb40f818f48bfbaf0b8824ef3f2356c39e4bb89e
Se0 <sy.park.HUB@gmail.com>
2022-02-13 13:32:04 +0900

    simpleheader 수정 path를 통해 클릭하면 현재 위치 bold 되는 기능 추가

--- src/components/layout/simpleheader.vue
+++ src/components/layout/simpleheader.vue
@@ -3,33 +3,60 @@
     <div id="header-box">
       <nav>
         <ul>
-         <router-link to="/stretchingGuide" class="nav-item">
-            <li class="nav-text">스트레칭 가이드</li>
-         </router-link>
+          <router-link to="/stretchingGuide" class="nav-item">
+            <li class="nav-text" id="stretchingGuide">스트레칭 가이드</li>
+          </router-link>
           <router-link to="/myroutine" class="nav-item">
-            <li class="nav-text">마이 루틴</li>
+            <li class="nav-text" id="myroutine">마이 루틴</li>
           </router-link>
           <router-link to="/story" class="nav-item">
-            <li class="nav-text">프로젝트 소개</li>
+            <li class="nav-text" id="story">프로젝트 소개</li>
           </router-link>
         </ul>
       </nav>
-      <a href="/"><img class="logo" src="..\..\assets\images\logo\logo.svg"/></a>
+      <a href="/"><img class="logo" src="..\..\assets\images\logo\logo.svg" alt="BTP 메인 이동 로고"/></a>
       <div class="my-menu">
-        <a href="#"><img src="../../assets/icon/mypage.svg"/></a>
-        <a href="#"><img src="../../assets/icon/notification.svg"/></a>
+        <router-link to="/account"
+          ><img src="../../assets/icon/mypage.svg"
+        /></router-link>
+        <router-link to="#"
+          ><img src="../../assets/icon/notification.svg"
+        /></router-link>
       </div>
     </div>
   </header>
-  <!-- 현재 페이지에  id="now-page" -->
 </template>
-
+<style lang="scss">
+@import "../../assets/scss/variables.scss";
+@import "../../assets/scss/common.scss";
+@import "simpleheader.scss";
+</style>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      pathname: "",
+      idname: "",
+    };
+  },
+  created: function() {
+    this.getPathname();
+  },
+  mounted: function() {
+    this.makeBold();
+  },
+  methods: {
+    getPathname() {
+      this.pathname = window.location.pathname.split("/")[1];
+      console.log(this.pathname);
+    },
+    makeBold() {
+      // pathname이 li id랑 똑같을때 bold
+      const boldli = document.getElementById(this.pathname);
+      boldli.id = "now-page";
+      console.log("boldli:", boldli);
+    },
+  },
+};
 </script>
 
-<style lang="scss">
-@import '../../assets/scss/variables.scss';
-@import '../../assets/scss/common.scss';
-@import 'simpleheader.scss';
-</style>
