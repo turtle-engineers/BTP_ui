d36752ce6c523ed414f2a7d192d82c39765eb428
mul <77082685+moonmul@users.noreply.github.com>
2022-02-24 21:44:10 +0900

    Update router.js

--- src/router.js
+++ src/router.js
@@ -11,33 +11,36 @@ import MissionList from "./views/MissionList";
 import About from "./views/About";
 import Story from "./views/Story";
 import NotFoundPage from "./views/NotFoundPage";
-import LoginElement from "./views/LoginElement";
-
+import LoginElement from "./views/LoginElement.vue";
+import Login from "./views/Login.vue";
+import LoginRedirect from "./views/LoginRedirect.vue";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    mode: "history",
-    routes: [
-        {path:"/", component: Home},
+  mode: "history",
+  routes: [
+    { path: "/", component: Home },
 
-        {path:"/myroutine", component: MyRoutine},
+    { path: "/myroutine", component: MyRoutine },
 
-        {path:"/randomChallenge", component: RandomChallenge},
-        {path:"/myturtle", component: MyTurtle},
-        {path:"/stretchingGuide", component: StretchingGuide},
+    { path: "/randomChallenge", component: RandomChallenge },
+    { path: "/myturtle", component: MyTurtle },
+    { path: "/stretchingGuide", component: StretchingGuide },
 
-        {path:"/account", component: Account},
-        {path:"/settings", component: Settings},
-        {path:"/missionList", component: MissionList},
+    { path: "/account", component: Account },
+    { path: "/settings", component: Settings },
+    { path: "/missionList", component: MissionList },
 
-        {path:"/about", component: About},        
-        {path:"/story", component: Story},
+    { path: "/about", component: About },
+    { path: "/story", component: Story },
 
-        {path:"/LoginElement", component: LoginElement},
+    { path: "/LoginElement", component: LoginElement },
+    { path: "/Login", component: Login },
+    { path: "/LoginRedirect", component: LoginRedirect },
 
-        {path:"*", component: NotFoundPage}
-    ]
+    { path: "*", component: NotFoundPage },
+  ],
 });
 
 export default router;
