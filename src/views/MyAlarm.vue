52c5c856acd13369edb30d18556a67a2ad495bbe
Se0 <sy.park.HUB@gmail.com>
2022-03-02 15:17:39 +0900

    alarm init MyAlarm.vue 추가 myAlarm.scss 추가 router에 Alarm 추가

---
+++ src/views/MyAlarm.vue
@@ -0,0 +1,13 @@
+<template>
+  <body>
+    <h1 class="btp-title">알람</h1>
+  </body>
+</template>
+<style lang="scss" scoped>
+@import "@/assets/scss/variables.scss";
+@import "@/assets/scss/common.scss";
+@import "@/assets/scss/components/myAlarm.scss";
+</style>
+<script>
+export default {};
+</script>
