6d16ca8a12f219e6645d89aa8561616b3ba509bf
Se0 <sy.park.HUB@gmail.com>
2022-03-15 14:35:46 +0900

    weekday 달성표 체크란

--- src/views/ProjectAttendance.vue
+++ src/views/ProjectAttendance.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>
+  <div class="app-container">
     <simpleheader></simpleheader>
-    <div>
-      <h1>프로젝트 참여표</h1>
-      <p>스트레칭을 완료했어요!</p>
+    <div class="btp-container">
+      <!-- 타이틀 -->
+      <div class="btp-title">
+        <p>스트레칭을 완료했어요!</p>
+        <h1>프로젝트 참여표</h1>
+      </div>
+      <!-- 참여표 -->
+      <section class="attendance-wrap">
+        <!-- week check -->
+        <div class="weekcheck">
+          <!-- TODO : month 추가 -->
+          <h1>11월 둘째 주</h1>
+          <article class="weekcheck" onclick="return false;">
+            <!-- v-on:change="updateIsChecked" -->
+            <input type="checkbox" value="0" id="mon" name="dayAlarmOnOff" />
+            <label class="weekday" for="mon">월</label>
+            <input type="checkbox" value="1" id="tue" name="dayAlarmOnOff" />
+            <label class="weekday" for="tue">화</label>
+            <input type="checkbox" value="2" id="wed" name="dayAlarmOnOff" />
+            <label class="weekday" for="wed">수</label>
+            <input type="checkbox" value="3" id="thu" name="dayAlarmOnOff" />
+            <label class="weekday" for="thu">목</label>
+            <input type="checkbox" value="4" id="fri" name="dayAlarmOnOff" />
+            <label class="weekday" for="fri">금</label>
+            <input type="checkbox" value="5" id="sat" name="dayAlarmOnOff" />
+            <label class="weekday" for="sat">토</label>
+            <input type="checkbox" value="6" id="sun" name="dayAlarmOnOff" />
+            <label class="weekday" for="sun">일</label>
+          </article>
+        </div>
+      </section>
     </div>
   </div>
 </template>
