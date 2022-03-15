e70e54f53548a85935e9b019d50c76f6cd95f83d
Se0 <sy.park.HUB@gmail.com>
2022-03-15 13:16:58 +0900

    프로젝트 참여표 컴포넌트 추가 컴포넌트 simpleheader 추가 scss 이름 오타 수정 script 수정

--- src/views/ProjectAttendance.vue
+++ src/views/ProjectAttendance.vue
@@ -1,11 +1,25 @@
 <template>
+  <div>
+    <simpleheader></simpleheader>
     <div>
-        <h1>프로젝트 참여표</h1>
-        <p>스트레칭을 완료했어요!</p>
+      <h1>프로젝트 참여표</h1>
+      <p>스트레칭을 완료했어요!</p>
     </div>
+  </div>
 </template>
+<style lang="scss" scoped>
+@import "../assets/scss/variables.scss";
+@import "../assets/scss/common.scss";
+@import "../assets/scss/components/ProjectAttendance.scss";
+</style>
 <script>
+import simpleheader from "../components/layout/simpleheader.vue";
 export default {
-    
+  data() {
+    return {};
+  },
+  components: {
+    simpleheader,
+  },
 };
 </script>
