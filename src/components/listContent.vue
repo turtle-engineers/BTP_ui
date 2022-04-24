b1bcb20455047567abd74d23a70097328037eea0
Se0 <sy.park.HUB@gmail.com>
2022-04-24 19:46:34 +0900

    스트레칭 리스트 내부 컨텐츠

---
+++ src/components/listContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="content-wrap">
+    <img src="../assets/images/stretching_sample.png" alt="sample img" />
+    <div class="caption-wrap">
+      <img src="../assets/images/bookmark-off.png" alt="bookmark-on" />
+      <div>
+        <h1>스트레칭 이름</h1>
+        <p>00 : 00 : 00</p>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import "../assets/scss/variables.scss";
+@import "../assets/scss/common.scss";
+@import "../assets/scss/components/listContent.scss";
+</style>
+<script>
+export default {};
+</script>
