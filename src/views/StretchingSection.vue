89596607fc3fc9ad930b7612288005771564eb3e
Se0 <sy.park.HUB@gmail.com>
2022-05-02 14:31:45 +0900

    마이루틴 스트레칭 진행도

--- src/views/StretchingSection.vue
+++ src/views/StretchingSection.vue
@@ -2,6 +2,23 @@
   <div class="app-container">
     <simpleheader></simpleheader>
     <div class="btp-container">
+      <section>
+        <div class="progress-time">
+          <h1>마이루틴 스트레칭 진행률</h1>
+          <p>00 : 00 / 99 : 99</p>
+        </div>
+        <!-- 여러개를 추가해도 일정하도록 코딩.
+        단, 루틴 갯수를 가져와서 
+        progress를 n번 추가하도록 코드를 짜야함.-->
+        <div class="progress">
+          <progress value="100" max="100"></progress>
+          <progress value="100" max="100"></progress>
+          <progress value="100" max="100"></progress>
+          <progress value="0" max="100"></progress>
+          <progress value="0" max="100"></progress>
+          <progress value="0" max="100"></progress>
+        </div>
+      </section>
       <section class="btp-title">
         <p>목 스트레칭</p>
         <h1>목돌리기 스트레칭</h1>
