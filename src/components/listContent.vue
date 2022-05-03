878fb2aa8562819bea7630e1c0d64b6b170f7c0b
Se0 <sy.park.HUB@gmail.com>
2022-05-03 21:23:56 +0900

    list Content 북마크 onoff 추가

--- src/components/listContent.vue
+++ src/components/listContent.vue
@@ -2,7 +2,7 @@
   <div class="content-wrap">
     <img src="../assets/images/stretching_sample.png" alt="sample img" />
     <div class="caption-wrap">
-      <img src="../assets/images/bookmark-off.png" alt="bookmark-on" />
+      <img :src="require(`../assets/images/bookmark-${bookmarkChecked}.png`)" alt="bookmark-on" v-on:click="bookmarkOnOff" />
       <div>
         <h1>스트레칭 이름</h1>
         <p>00 : 00 : 00</p>
@@ -11,10 +11,25 @@
   </div>
 </template>
 <style lang="scss" scoped>
-@import "../assets/scss/variables.scss";
-@import "../assets/scss/common.scss";
-@import "../assets/scss/components/listContent.scss";
+@import '../assets/scss/variables.scss';
+@import '../assets/scss/common.scss';
+@import '../assets/scss/components/listContent.scss';
 </style>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      bookmarkChecked : 'off'
+    };
+  },
+  methods: {
+    bookmarkOnOff() {
+      if(this.bookmarkChecked == 'off'){
+        this.bookmarkChecked = 'on'
+      }else{
+        this.bookmarkChecked = 'off'
+      }
+    },
+  },
+};
 </script>
