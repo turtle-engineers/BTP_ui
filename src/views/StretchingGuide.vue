385e09d721be7ed41618b5c51c0b3020a2b74ef2
Se0 <sy.park.HUB@gmail.com>
2022-05-24 00:44:03 +0900

    스트레칭 가이드 페이지

--- src/views/StretchingGuide.vue
+++ src/views/StretchingGuide.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>
+  <div class="app-container">
+    <simpleheader></simpleheader>
+    <div class="btp-container">
+      <!-- 상단 타이틀 + 시간/시작 -->
+      <section class="btp-title">
         <h1>스트레칭 가이드</h1>
+        <p>
+          거북이에서 다시 인간이 되기 위해 스트레칭을 따라해보세요.
+        </p>
+      </section>
+      <!-- 스트레칭 메뉴 -->
+      <StretchList></StretchList>
     </div>
+  </div>
 </template>
 <script>
+import simpleheader from "../components/layout/simpleheader.vue";
+import StretchList from '../components/StretchList.vue';
 export default {
-    
+  components: {
+    simpleheader,
+    StretchList,
+  },
 };
 </script>
+<style>
+</style>
