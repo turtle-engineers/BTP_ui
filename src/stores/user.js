860b08f811673a289f66e91882713373e2e2d039
Se0 <sy.park.HUB@gmail.com>
2022-07-19 20:17:28 +0900

    user store 추가 user 정보, getInfo, 로그인페이지 이동, 카카오로그인 이동

---
+++ src/stores/user.js
@@ -0,0 +1,46 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+export const userStore = defineStore('userStore', {
+  state: () => {
+    return {
+      isLogin: false,
+      results: {
+        id: '',
+        provider: '',
+        providerId: '',
+        picture: '',
+        nickname: '',
+        title: '',
+        level: '',
+        point: '',
+        createdAt: '',
+        updatedAt: '',
+        alarmValid: '',
+        monthTimes: '',
+        dd: '',
+        todayTimes: '',
+      },
+    };
+  },
+  getters: {},
+  actions: {
+    getUserInfo() {
+      axios.get('http://127.0.0.1:3000/user/info').then((res) => {
+        if (res.data.result != null) {
+          console.log(res.data);
+          this.results = res.data.results;
+          this.isLogin = true;
+          //   this.$router.push("/");
+        }
+        //   console.log(res);
+      });
+    },
+    moveTologinPage() {
+      window.location.replace('/login');
+    },
+    loginAtBackServer() {
+      window.location.replace('http://127.0.0.1:3000/oauth/kakao');
+    },
+  },
+});
